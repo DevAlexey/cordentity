10eeb77a356f0eb1903a0d722fad687a32b22afc
Alexander Vtyurin <senior.joinu@gmail.com>
2019-03-26 16:22:52 +0300

    remove integration delay, improve integration test
    Signed-off-by: Alexander Vtyurin <senior.joinu@gmail.com>

--- integration/src/test/kotlin/com/luxoft/blockchainlab/IntegrationTest.kt
+++ integration/src/test/kotlin/com/luxoft/blockchainlab/IntegrationTest.kt
@@ -1,4 +1,13 @@
 package com.luxoft.blockchainlab
 
+import com.luxoft.blockchainlab.hyperledger.indy.utils.SerializationUtils
+import org.junit.Test
+
 class IntegrationTest {
+    @Test
+    fun `integrates correctly`() {
+        // yes if can import classes from cordentity
+        val yes = SerializationUtils.anyToJSON("YES")
+        assert(SerializationUtils.jSONToAny<String>(yes) == "YES")
+    }
 }
